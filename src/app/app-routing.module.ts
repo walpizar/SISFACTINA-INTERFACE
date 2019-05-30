f949986ab4a573050dce6199d7273b9e3ca7e3d9
Antony Montero Salas <antoloy19@hotmail.com>
2019-05-30 14:43:37 -0600

    errores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,11 +10,12 @@ import { DocumentsComponent } from './documents/documents.component';
 import { DocumentsDetailsComponent } from './Documents/documents-details/documents-details.component';
 import { EmpresasComponent } from './empresas/empresas.component';
 import { ListaEmpresaComponent } from './Empresas/lista-empresa.component';
-<<<<<<< HEAD
 import { IndexCategoriaProductoComponent } from './CategoriaProducto/index-categoria-producto/index-categoria-producto.component';
 import { DetalleCategoriaProductoComponent } from './CategoriaProducto/detalle-categoria-producto/detalle-categoria-producto.component';
 import { CrearCategoriaProductoComponent } from './CategoriaProducto/crear-categoria-producto/crear-categoria-producto.component';
-
+import { ListaCliComponent } from './Clientes/lista-cli/lista-cli.component';
+import { RegistroCliComponent } from './Clientes/registro-cli/registro-cli.component';
+import { DetalleCliComponent } from './Clientes/detalle-cli/detalle-cli.component';
 const routes: Routes = [
   {path:'Indexabono',component:IndexAbonoComponent},
   {path:'detalle', component:DetallesAbonosComponent },
@@ -27,27 +28,13 @@ const routes: Routes = [
   {path:'empresa',component:ListaEmpresaComponent},
   {path:'Indexcategoriaproduct',component:IndexCategoriaProductoComponent},
   {path:'detailscateproduct',component:DetalleCategoriaProductoComponent},
-  {path:'createcateproduct',component:CrearCategoriaProductoComponent}
-=======
-import { ListaCliComponent } from './Clientes/lista-cli/lista-cli.component';
-import { RegistroCliComponent } from './Clientes/registro-cli/registro-cli.component';
-import { DetalleCliComponent } from './Clientes/detalle-cli/detalle-cli.component';
+  {path:'createcateproduct',component:CrearCategoriaProductoComponent},
+  {path: 'ListaClientes', component: ListaCliComponent },
+  {path: 'RegistroCliente', component: RegistroCliComponent },
+  {path: 'DetallesCliente/:id', component: DetalleCliComponent }
 
-const routes: Routes = [
-  { path: 'Indexabono', component: IndexAbonoComponent },
-  { path: 'detalle', component: DetallesAbonosComponent },
-  { path: 'abo', component: AbonosComponent },
-  { path: 'proveedor', component: ProveedorComponent },
-  { path: 'Indexproveedor', component: IndexProveedorComponent },
-  { path: 'detalleproveedor', component: DetalleProveedorComponent },
-  { path: 'factura', component: DocumentsComponent },
-  { path: 'detalleFactura/:id', component: DocumentsDetailsComponent },
-  { path: 'empresa', component: ListaEmpresaComponent },
-  { path: 'ListaClientes', component: ListaCliComponent },
-  { path: 'RegistroCliente', component: RegistroCliComponent },
-  { path: 'DetallesCliente/:id', component: DetalleCliComponent }
->>>>>>> 73056f1d8866440d84d1dc72206dd63db8291cd4
-];
+]
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
