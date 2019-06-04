d8787733a115419a906c59bb2224f079b1e1a500
Mariela Gutierrez <marielex@hotmail.es>
2019-06-04 14:27:53 -0600

    PersonaTribunal

---
+++ src/Models/PersonaTribunal.ts
@@ -0,0 +1,11 @@
+export class TbPersonasTribunalS {
+    Id:string;
+    Codigopostal:string;
+    Sexo:string;
+    Nombre:string;
+    Apellido1:string;
+    Apellido2:string;
+
+    constructor(){
+    }
+}
